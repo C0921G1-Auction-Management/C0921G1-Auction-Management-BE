c09d37cc61b3f7a7e4895542321d218db1727452
Wiz2101 <trancongdat.2101@gmail.com>
2022-04-25 00:37:24 +0700

    update list product

--- src/main/java/vn/codegym/com/c0921g1_sprint2/service/impl/ProductServiceImpl.java
+++ src/main/java/vn/codegym/com/c0921g1_sprint2/service/impl/ProductServiceImpl.java
@@ -1,6 +1,19 @@
 package vn.codegym.com.c0921g1_sprint2.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import vn.codegym.com.c0921g1_sprint2.model.Product;
+import vn.codegym.com.c0921g1_sprint2.repository.ProductRepository;
 import vn.codegym.com.c0921g1_sprint2.service.ProductService;
-
+@Service
 public class ProductServiceImpl implements ProductService {
+    @Autowired
+    ProductRepository productRepository;
+    /*DatTC - Lấy list sản phẩm trang chủ*/
+    @Override
+    public Page<Product> getAll(String name, String category, String price1, String price2, Pageable pageable) {
+        return productRepository.findProductByOption(name, category, price1, price2, pageable);
+    }
 }
